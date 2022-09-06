bcc498e2396ec2cfaa15eedccb4d2412d8e4cbe5
Felipe <pgatilo20@gmail.com>
2022-09-06 17:33:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState} from 'react'
+import {Formik, Form, Field} from 'formik'
+import './Header.css'
+import './Content.css'
+import './article.css'
+
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url =>  window.open(url)
+  console.log({photos})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <div>
+    <h1 className='h1'>Buscador de Imagenes</h1>
+      <header>
+        
+        <Formik
+          initialValues = {{ search: ''}}
+          onSubmit = {async values => {
+            //llamar a api de unsplash
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, 
+            {
+            headers: {
+              'Authorization': 'Client-ID nbVh9mDJrR-a38obSJAQzfZwUBoXZYZB5KhOiLkJEGo',
+            }
+          })
+           const data = await response.json()
+           setPhotos(data.results)
+          }}
+          >
+            <Form>
+              <Field name='search' />
+            </Form>
+        </Formik>
       </header>
-    </div>
+      <div className='container '> 
+          <div className='center'>
+            {photos.map(photo => <article key={photo.id} onClick={()=> open(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>)}
+          </div>
+      </div>
+   </div>
   );
 }
 
